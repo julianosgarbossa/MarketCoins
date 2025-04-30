67d3720e2f19a4c42ef849919466c17ca7e81cd6
Juliano Sgarbossa <contato.julianosgarbossa@gmail.com>
2025-04-30 19:03:44 -0300

    Scena CoinsList VIP
    Criando scena coinsList com arquitetura VIP

--- MarketCoins/ViewController.swift
+++ MarketCoins/ViewController.swift
@@ -8,25 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let dataProvider = ListCoinsDataProvider()
-        dataProvider.delegate = self
-        dataProvider.fetchListCoins(by: "brl", with: nil, orderBy: "market_cap.desc", total: 10, page: 1, percentagePrice: "1h")
     }
-}
-
-extension ViewController: ListCoinsDataProviderDelegate {
-    func sucess(model: Any) {
-        let coinList = model as? [CoinModel]
-        print(coinList ?? "Vazio")
-    }
-    
-    func errorData(_ provider: (any GenericDataProviderDelegate)?, error: any Error) {
-        print(error.localizedDescription)
-    }
-    
-    
 }
 
